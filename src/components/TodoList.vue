89b1d001959d070e62974b3a3f4784e8bb7c42cd
Vishal <vishalbhola5@gmail.com>
2020-08-09 19:23:21 +0530

    Passing methods

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -16,6 +16,9 @@
             :key="index"
             :todoString="todo.todoString"
             :completed="todo.completed"
+            @on-delete="deleteTodo(todo)"
+            @on-toggle="toggleTodo(todo)"
+            @on-edit="editTodo(todo, $event)"
           />
         </ul>
       </div>
